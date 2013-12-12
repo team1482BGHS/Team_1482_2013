9101e01ddcd5ad797198c1f445eb5a0a8cb9b97c
nickmcski <Nick.mc.ski1@gmail.com>
2013-12-11 20:08:24 -0700

    Added variables to the config

--- src/org/mcmodding/team1482/Config.java
+++ src/org/mcmodding/team1482/Config.java
@@ -13,6 +13,8 @@ public class Config {
     public static final int GEARUP = 3700;
     public static final int GEARDOWN = 2800;
     public static final double STRAIGHTDEADZONE = 0.1; //Dead zone to be considered to be driveing "straight" enfough.
+    public static final double GEARCHANGESPEEDDEFAULT = 0.7;
+    public static final double GEARUPDELAY = 0.07;
     
     
 }
